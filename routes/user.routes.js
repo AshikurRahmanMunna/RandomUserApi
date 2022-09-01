11b5f46130a54abdb28e79e1d7cbcf48f4faf17d
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-09-02 01:47:50 +0600

    User save done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { getARandomUser, getAllUsers, saveAUser } = require('../controllers/user.controller');
+const { validateSaveUserRequest, isRequestValidated } = require('../validators/user.validators');
 
 router.get('/random', getARandomUser);
 router.get('/all', getAllUsers);
-router.post('/save', saveAUser);
+router.post('/save', validateSaveUserRequest, isRequestValidated, saveAUser);
 
 module.exports = router;
